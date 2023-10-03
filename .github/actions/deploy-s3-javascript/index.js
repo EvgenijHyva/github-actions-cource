334a8908e1fe51eaad4e4e6e7ac4f6f821627468
Evgeny <evgeny.hyvarinen@tietoevry.com>
2023-10-03 20:19:16 +0300

    syncronize folder with AWS, or in other words deploy artifact

--- .github/actions/deploy-s3-javascript/index.js
+++ .github/actions/deploy-s3-javascript/index.js
@@ -6,8 +6,15 @@ const exec = require('@actions/exec');
 const run = () => {
 	core.notice("Starting run workflow");
 	// Get input values
-	
+	const bucket = core.getInput('bucket', { trimWhitespace: true, required:true });
+	const region = core.getInput('bucket-region', { trimWhitespace: true, required:true });
+	const artifact = core.getInput('dist-folder', { required:true });
 
+	// Upload files (can be used Amazon-SDK or execute aws command)
+	const s3Uri = `s3://${bucket}`;
+	// `aws s3 sync <local-folder> <s3-bucket>`
+	exec.exec(`aws s3 sync ${artifact} ${s3Uri} --region ${region}`); // syncronize the folder
+	core.notice("Finish workflow");
 }
 
 run();
