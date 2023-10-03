bb4e8109c97dbfabec9dfaf8552be6cef9385f06
Evgeny <evgeny.hyvarinen@tietoevry.com>
2023-10-03 21:03:11 +0300

    refactoring for async function

--- .github/actions/deploy-s3-javascript/index.js
+++ .github/actions/deploy-s3-javascript/index.js
@@ -3,7 +3,7 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 
-const run = () => {
+const run = async () => {
 	core.notice("Starting run workflow");
 	// Get input values
 	const bucket = core.getInput('bucket', { trimWhitespace: true, required:true });
@@ -13,11 +13,11 @@ const run = () => {
 	// Upload files (can be used Amazon-SDK or execute aws command)
 	const s3Uri = `s3://${bucket}`;
 	// `aws s3 sync <local-folder> <s3-bucket>`
-	exec.exec(`aws s3 sync ${artifact} ${s3Uri} --region ${region}`); // syncronize the folder
+	await exec.exec(`aws s3 sync ${artifact} ${s3Uri} --region ${region}`); // syncronize the folder
 
-	const config = exec.exec(`aws s3api get-bucket-website --bucket ${bucket}`);
+	const config = await exec.exec(`aws s3api get-bucket-website --bucket ${bucket}`);
 	core.notice(`Deployed website configurations: ${config}`);
-	
+
 	const endpoint = JSON.parse(config).Endpoint;
 	core.notice(`Website is awailable on url:\n ${endpoint}`);
 
