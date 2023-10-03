151c33700ff788df593855224a673869b6200999
Evgeny <evgeny.hyvarinen@tietoevry.com>
2023-10-03 23:56:19 +0300

    Added output

--- .github/actions/deploy-s3-javascript/index.js
+++ .github/actions/deploy-s3-javascript/index.js
@@ -4,7 +4,6 @@ const exec = require('@actions/exec');
 
 
 const run = async () => {
-	core.notice("Starting run workflow");
 	// Get input values
 	const bucket = core.getInput('bucket', { trimWhitespace: true, required:true });
 	const region = core.getInput('bucket-region', { trimWhitespace: true, required:true });
@@ -15,10 +14,9 @@ const run = async () => {
 	// `aws s3 sync <local-folder> <s3-bucket>`
 	await exec.exec(`aws s3 sync ${artifact} ${s3Uri} --region ${region}`); // syncronize the folder
 
-	const resultConf = await exec.exec(`aws s3api get-bucket-website --bucket ${bucket}`);
-	core.notice(`Config: ${JSON.stringify(resultConf)}`)
-
-	core.notice("Finish workflow");
+	const websiteUrl = `http://${bucket}.s3-website-${region}.amazonaws.com`;
+	core.setOutput('website-url', websiteUrl);
+	core.notice(`Finishing workflow. Website Url: ${websiteUrl}`);
 }
 
 run();
